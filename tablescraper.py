131079b0385602ec1d7fe456944b719b472c5604
IanLondon <ianlondondu@gmail.com>
2016-04-14 22:16:14 -0400

    Implement scrape_multi_elems

--- tablescraper.py
+++ tablescraper.py
@@ -6,16 +6,38 @@
 from lxml import html
 from lxml.etree import tostring as html_to_string
 import requests
-import pandas as pd
 import urlparse
+
+import pandas as pd
+
 import logging
+import logging.config
+
+
+# Set up logging
+logger = logging.getLogger(__name__)
+
+fhandler = logging.FileHandler(filename='tablescraper.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fhandler.setFormatter(formatter)
+logger.addHandler(fhandler)
+logger.setLevel(logging.DEBUG)
+# prevent logging bubbling up to iPython Notebook
+logger.propagate = False
+
 
 def to_sequence(x):
     """
     Makes sure an object is a sequence and if it's not, return a 1-tuple containing that object.
     Useful if you want to take an argument which might be one element and might be a list or tuple
+    'foo' -> ['foo']
+    # other objects like lists, tuples, np.array, dict, etc:
+    [1,2,3] -> [1,2,3]
+    np.array([1,2,3]) -> np.array([1,2,3])
+    {0:42} -> {0:42}
+    # etc
     """
-    if isinstance(x, list) or isinstance(x, tuple):
+    if hasattr(x, '__iter__'):
         return x
     return (x,)
 
@@ -23,11 +45,11 @@ def url_to_tree(url):
     try:
         page = requests.get(url)
     except requests.ConnectionError as e:
-        logging.error('Could not connect to url {url}: {e}'.format(url=url,e=e))
+        logger.error('Could not connect to url {url}: {e}'.format(url=url,e=e))
         return
 
     if page.status_code != 200:
-        logging.error('{res}: could not get url {url}'.format(res=page.response, url=url))
+        logger.error('{res}: could not get url {url}'.format(res=page.response, url=url))
         return
 
     tree = html.fromstring(page.content)
@@ -59,21 +81,68 @@ def dfs_from_tables(table_els, header=None, skiprows=1):
     return dfs
 
 
-def scrape_elems(urls, elem_xpath, elem_index=0, header=None):
+def scrape_multi_elems(keyed_urls, scrape_dict):
+    """
+    Arguments:
+        url_label: a dict containing URL strings keyed by your url labels, eg:
+            {'banana':'http://www.fruits.com/banana', 'orange':'http://www.fruits.com/orange'}
+
+        scrape_dict: a dict containing xpaths keyed by your element labels. eg:
+            {'calories':'xpath_selector1', 'fiber':'xpath_selector2', ...}
+
+    Returns:
+        final_results: a dict of dicts. The first key corresponds to the url label
+        and the second key corresponds to the element label.
+
+        Example -- from the banana page, get the 0th 'calories' element:
+            final_results['banana']['calories'][0]
+
+    Example:
+        >>> scrape_multi_elems({'examp':'http://www.example.com'},{'txt':'//h1/text()','p':'//p/text()'})
+        {'examp': {'p': ['This domain is established to be used for illustrative examples in documents. \
+        You may use this\n    domain in examples without prior coordination or asking for permission.'],
+        'txt': ['Example Domain']}}
+
+    """
+    final_results = {}
+    page_results = {}
+
+    logger.info('Beginning new multi-scrape of %i urls' % len(keyed_urls))
+
+    for url_label, url in keyed_urls.iteritems():
+        tree = url_to_tree(url)
+
+        for el_label, xpath in scrape_dict.iteritems():
+            all_elems = tree.xpath(xpath)
+            logger.info('multiscrape got %i elements with xpath %s for url %s' % (len(all_elems), xpath, url))
+
+            page_results[el_label] = all_elems
+
+        final_results[url_label] = page_results
+
+    return final_results
+
+def scrape_elems(urls, elem_xpath, elem_index=0, fail_val=None):
     final_elems = []
 
     urls = to_sequence(urls)
+
+    logger.info('Beginning new scrape of %i urls' % len(urls))
 
     for url in urls:
         tree = url_to_tree(url)
 
         # select the specified elem element
         all_elems = tree.xpath(elem_xpath)
-        logging.info('Got %i elements with xpath %s for url %s' % (len(all_elems), elem_xpath, url))
+        logger.info('Got %i elements with xpath %s for url %s' % (len(all_elems), elem_xpath, url))
 
         if elem_index is not None:
-            logging.info('selected the %ith' % elem_index)
-            elem_el = all_elems[elem_index]
+            try:
+                logger.info('selected the %ith' % elem_index)
+                elem_el = all_elems[elem_index]
+            except IndexError:
+                logger.warn('element index out of range, falling back to fail_val')
+                elem_el = fail_val
 
         final_elems.append(elem_el)
 
